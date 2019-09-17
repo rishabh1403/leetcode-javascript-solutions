70298a741cae3dfb7e51b62dd080717ff1617383
Rishabh Jain <mailrishabh1403@gmail.com>
2019-09-17 14:29:10 +0530

    add reverse integer

---
+++ 1-100/7-reverse-integer.js
@@ -0,0 +1,46 @@
+// string based method
+
+var reverse = function (x) {
+
+  const reversedInt = parseInt(Math.abs(x).toString().split('').reverse().join(''));
+
+  if (reversedInt > 2 ** 31) return 0;
+
+  return reversedInt * Math.sign(x);
+
+};
+
+// string based method without explicit type casting
+
+var reverse = function (x) {
+
+  const reversedInt = Math.abs(x).toString().split('').reverse().join('');
+
+  if (reversedInt > 2 ** 31) return 0;
+
+  return reversedInt * Math.sign(x);
+
+};
+
+// integer based method
+
+var reverse = function (x) {
+  const isNegative = x < 0 ? true : false;
+
+  if (isNegative) {
+    x = x * -1;
+  }
+
+  let reversed = 0;
+  while (x > 0) {
+    reversed = (reversed * 10) + (x % 10);
+
+    x = parseInt(x / 10);
+  }
+
+  if (reversed > 2 ** 31) {
+    return 0;
+  }
+
+  return isNegative ? reversed * -1 : reversed;
+};
