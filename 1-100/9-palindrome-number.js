6b4998f86585fbf9e9960662d4fd58f15c5ef375
Rishabh Jain <mailrishabh1403@gmail.com>
2019-09-17 16:15:44 +0530

    add question 9

---
+++ 1-100/9-palindrome-number.js
@@ -0,0 +1,56 @@
+// string based implementation
+
+var isPalindrome = function (x) {
+  return x == x.toString().split('').reverse().join('');
+};
+
+// number based implementation
+
+var isPalindrome = function (x) {
+  const isNegative = x < 0 ? true : false;
+
+  if (isNegative) {
+    return false;
+  }
+
+  const temp = x;
+  let reversed = 0;
+
+  while (x > 0) {
+    reversed = (reversed * 10) + (x % 10);
+    x = parseInt(x / 10);
+  }
+
+  return reversed == temp;
+};
+
+// two pointer method
+
+var isPalindrome = function (x) {
+
+  if (x < 0) {
+    return false;
+  }
+
+  if (x < 10) {
+    return true;
+  }
+
+  if (x % 10 === 0 && x !== 0) {
+    return false;
+  }
+
+  const str = String(x);
+  let i = 0, j = str.length - 1;
+
+  while (i < j) {
+    if (str[i] !== str[j]) {
+      return false;
+    }
+
+    i++;
+    j--;
+  }
+
+  return true;
+};
