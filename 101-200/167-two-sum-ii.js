2d05845894b2a069762bc020d74870886f349865
Rishabh Jain <mailrishabh1403@gmail.com>
2019-09-30 17:40:31 +0530

    add q167

---
+++ 101-200/167-two-sum-ii.js
@@ -0,0 +1,20 @@
+/*
+Author :- Rishabh Jain <contact@rishabh1403.com>
+Solution for :- https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+blog for this code :- https://rishabh1403.com/leetcode-solution-of-two-sum-ii-in-javascript
+*/
+
+
+var twoSum = function (numbers, target) {
+  let i = 0;
+  let j = numbers.length - 1;
+  while (i !== j) {
+    if (numbers[i] + numbers[j] > target) {
+      j--;
+    } else if (numbers[i] + numbers[j] < target) {
+      i++;
+    } else {
+      return [i + 1, j + 1];
+    }
+  }
+};
