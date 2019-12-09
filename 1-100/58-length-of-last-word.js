1c87c9f8ba19e7fed95d1da718059fb6b2d4d746
Rishabh Jain <mailrishabh1403@gmail.com>
2019-12-09 18:03:33 +0530

    add yt video for two sum prob and reverse integer

--- 1-100/58-length-of-last-word.js
+++ 1-100/58-length-of-last-word.js
@@ -10,30 +10,38 @@ var lengthOfLastWord = function (s) {
   return words[words.length - 1].length;
 };
 
-
 // Back traversal
-var lengthOfLastWord = function(s) {
+
+var lengthOfLastWord = function (s) {
   let len = 0;
   let hasStarted = false;
+
   for (let i = s.length - 1; i >= 0; i--) {
-      if (s[i] !== ' ') hasStarted = true;
-      if (hasStarted) {
-          if (s[i] === ' ') break;
-          len++;
+    if (s[i] !== ' ') {
+      hasStarted = true;
+    }
+
+    if (hasStarted) {
+      if (s[i] === ' ') {
+        break;
       }
+
+      len++;
+    }
   }
   return len;
 };
 
 // Back traversal with library menthods
 
-var lengthOfLastWord = function(s) {
+var lengthOfLastWord = function (s) {
   s = ' ' + s.trim();
   return s.length - s.lastIndexOf(' ') - 1;
 };
 
+
 // regex
 
-var lengthOfLastWord = function(s) {
+var lengthOfLastWord = function (s) {
   return (s.match(/(\w+)\s*$/) || [, ''])[1].length;
 };
