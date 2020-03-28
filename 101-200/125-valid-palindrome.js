660b8d32666cd9aa0f17d543771a62ae643d5b31
Rishabh Jain <mailrishabh1403@gmail.com>
2020-03-28 11:12:43 +0530

    add valid palindrome draft

---
+++ 101-200/125-valid-palindrome.js
@@ -0,0 +1,30 @@
+/*
+Author :- Rishabh Jain <contact@rishabh1403.com>
+Solution for :- https://leetcode.com/problems/valid-palindrome/
+blog for this code :- https://rishabh1403.com/posts/coding/leetcode/2020/03/leetcode-search-insert-position
+youtube video :- https://youtu.be/l2XPvyTlC6c
+*/
+
+var isPalindrome = function (s) {
+  if (s.length === 0) return true;
+
+  s = s.toLowerCase();
+  let i = 0, j = s.length - 1;
+  while (i < j) {
+    if ((s[i] < 'a' || s[i] > 'z') && (s[i] < '0' || s[i] > '9')) {
+      i++;
+      continue;
+    }
+    if ((s[j] < 'a' || s[j] > 'z') && (s[j] < '0' || s[j] > '9')) {
+      j--;
+      continue;
+    }
+    if (s[i] !== s[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
+
+  return true;
+};
