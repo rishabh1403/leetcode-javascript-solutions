56c04291055037f1976acf6507717e9e5522e9e4
Rishabh Jain <mailrishabh1403@gmail.com>
2020-03-29 12:34:22 +0530

    add question 1365

--- 1301-1400/1365-how-many-numbers-smaller-than-current-number.js
+++ 1301-1400/1365-how-many-numbers-smaller-than-current-number.js
@@ -1,8 +1,8 @@
 /*
 Author :- Rishabh Jain <contact@rishabh1403.com>
-Solution for :- https://leetcode.com/problems/missing-number/
-blog for this code :- https://rishabh1403.com/posts/coding/leetcode/2020/03/leetcode-missing-number
-youtube video :- https://youtu.be/I6AUMvi13fc
+Solution for :- https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+blog for this code :- https://rishabh1403.com/posts/coding/leetcode/2020/03/leetcode-how-many-numbers-smaller-than-current-number
+youtube video :- https://youtu.be/a2RWy9kqOhs
 */
 
 
