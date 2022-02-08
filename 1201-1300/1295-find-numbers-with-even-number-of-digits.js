61c526315ce7d48f4e5cb577b55fd8b94d7afe7a
Rohitdubey2605 <36653420+Rohitdubey2605@users.noreply.github.com>
2022-02-08 23:50:00 +0530

    Update 1295-find-numbers-with-even-number-of-digits.js

--- 1201-1300/1295-find-numbers-with-even-number-of-digits.js
+++ 1201-1300/1295-find-numbers-with-even-number-of-digits.js
@@ -7,17 +7,13 @@ youtube video :- https://youtu.be/p9LaMHYY1R0
 
 
 var findNumbers = function (nums) {
-  let count = 0;
-  for (let num of nums) {
-    let digit = 0;
-
-    while (num > 0) {
-      digit++;
-      num = parseInt(num / 10);
-    }
-
-    if (digit % 2 === 0) count++;
-  }
-
-  return count;
+  let count =0;
+    nums.forEach((element) => {
+        if(element.toString().length%2==0)
+            {
+                count++;
+            }
+    });
+    
+    return count;
 };
